7cc2168b65a5e1ed335318767423b4a48df3ce0c
Karn Wong <karn@karnwong.me>
2024-04-03 11:08:14 +0700

    init authPayload

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kahnwong/subsonic-lyrics-fetcher
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+// init auth payload
+type AuthPayload struct {
+	u string
+	t string
+	s string
+	v string
+	c string
+	f string
+}
+
+func initAuthPayload() *AuthPayload {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Loading env from env var instead...")
+	}
+
+	return &AuthPayload{os.Getenv("USERNAME"), os.Getenv("TOKEN"), os.Getenv("SALT"), "1.16.1", "github-readme", "json"}
+}
+
+// get now playing
+//func getNowPlaying() string {
+//	return "foo"
+//}
 
 func main() {
+	authPayload := initAuthPayload()
 	fmt.Println("hello")
+	fmt.Println(authPayload)
 }
